c9763c5bfe32da6eb18767dee953be725555488d
Steve Rhoton <steve.rhoton@netdocuments.com>
2024-11-16 07:02:07 -0500

    Fix underscore

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "ecs-cluster-${var.environment}"
+  name = "ecs-cluster-${replace(var.environment, "_", "-")}"
 }
 
 resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_provider" {
@@ -32,7 +32,7 @@ resource "aws_iam_role_policy_attachment" "task_role_policy_attachment" {
 }
 
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family = "ecs-task-${var.environment}"
+  family = "ecs-task-${replace(var.environment, "_", "-")}"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "256"
@@ -60,7 +60,7 @@ EOF
 }
 
 resource "aws_lb" "ecs_lb" {
-  name = "ecs-lb-${var.environment}"
+  name = "ecs-lb-${replace(var.environment, "_", "-")}"
   internal = false
   load_balancer_type = "application"
   security_groups = [aws_security_group.ecs_sg.id]
@@ -68,7 +68,7 @@ resource "aws_lb" "ecs_lb" {
 }
 
 resource "aws_lb_target_group" "ecs_lb_target_group" {
-  name = "ecs-tg-${var.environment}"
+  name = "ecs-tg-${replace(var.environment, "_", "-")}"
   port = 80
   protocol = "HTTP"
   target_type = "ip"
@@ -88,7 +88,7 @@ resource "aws_lb_listener" "ecs_lb_listener" {
 
 
 resource "aws_ecs_service" "ecs_service" {
-  name = "ecs-service-${var.environment}"
+  name = "ecs-service-${replace(var.environment, "_", "-")}"
   cluster = aws_ecs_cluster.ecs_cluster.arn
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count = 1
@@ -105,7 +105,7 @@ resource "aws_ecs_service" "ecs_service" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name = "ecs-sg-${var.environment}"
+  name = "ecs-sg-${replace(var.environment, "_", "-")}"
   vpc_id = data.aws_vpc.main.id
 
   ingress {
